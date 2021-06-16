5eafe69d6dd2d9f4a3df0a9815aaec49d2fde192
Dharmika228 <61747322+Dharmika228@users.noreply.github.com>
2021-06-16 14:02:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -16,24 +16,6 @@ ssocket.bind(socket_address)
 ssocket.listen(10)
 print("LISTENING AT:",socket_address)
 
-#server side streaming
-
-import socket, cv2, pickle,struct,imutils
-
-ssocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-print('HOST IP:',host_ip)
-port = 9999
-socket_address = (host_ip,port)
-
-
-ssocket.bind(socket_address)
-
-
-ssocket.listen(10)
-print("LISTENING AT:",socket_address)
-
 
 while True:
     csocket,addr = ssocket.accept()
